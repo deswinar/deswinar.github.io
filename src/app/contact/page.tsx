e19ef1d159e7210586851a5f86a8c5eb6990804a
deswinar <deswinauliyaarabbani@gmail.com>
2024-12-21 14:33:23 +0000

    removing unnecessary files

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Feel free to reach out!</p>
+      {/* Add more contact information or a form here */}
+    </div>
+  );
+};
+
+export default ContactPage;
